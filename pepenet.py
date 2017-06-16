6b45cff63e86bcae33a937cab475d827d2283965
Jacotsu <Jacotsu@users.noreply.github.com>
2017-06-17 01:40:34 +0200

    Flak8d pepenet.py

--- pepenet.py
+++ pepenet.py
@@ -7,18 +7,19 @@ app = Flask(__name__)
 host1, port1 = 'localhost', 3000
 host2, port2 = 'localhost', 3001
 
+
 @app.route("/")
 def hello():
 
-    print (remoteDHT["my_key"])	# blocking get
-    remoteDHT.get ("my_key", lambda data: print(data)) # threaded get
+    print(remoteDHT["my_key"])	 # blocking get
+    remoteDHT.get("my_key", lambda data: print(data))  # threaded get
 
     return "Shadilay! fellow kekistani enjoy your"\
            " pepes {}".format(remoteDHT["my_key"])
 
 
 # the storage parameter allows us to cache the keys locally and persistently
-localDHT = DHT(host1, port1,storage=shelve.open ('pepes.ree'))
+localDHT = DHT(host1, port1, storage=shelve.open('pepes.ree'))
 
 # seeds is a list of tuples (host, port), which contains the bootstrap
 # nodes for our dht, we should load them from a file and update the at
@@ -30,4 +31,3 @@ localDHT["my_key"] = [u"Timestap",
                       u"Object"]
 # Returns your peers
 peers = localDHT.peers()
-
